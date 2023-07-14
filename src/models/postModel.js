aae817ee634400a745f02c8817c4e0ae596e456a
pareshdevatval1992 <paresh.devatval@sculptsoft.com>
2023-07-14 15:25:04 +0530

    added desc5

--- src/models/postModel.js
+++ src/models/postModel.js
@@ -8,7 +8,7 @@ const permissionsObject = {
 const postSchema = new mongoose.Schema({
     title: String,
     description: String,
-    description4: String,
+    description5: String,
     uid: {
        type: mongoose.Schema.Types.ObjectId,
        ref: "Users"
